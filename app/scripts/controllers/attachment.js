35119e3454a611ac2cc1440789aefe8ad5e6ef98
Delly Mellas Nyman <delly.nyman@sangkuriang.co.id>
2017-01-11 18:48:22 +0700

    upload attachment

--- app/scripts/controllers/attachment.js
+++ app/scripts/controllers/attachment.js
@@ -13,18 +13,52 @@ angular.module('pocApp')
       form.append("overwrite", "true");
 
       var settings = {
-        "async": true,
-        "crossDomain": true,
         "url": "http://10.3.3.66:8080/alfresco/service/api/upload?alf_ticket=TICKET_decec884d65414733c9ddca657df2085791567d6",
         "method": "POST",
-        "processData": false,
-        "contentType": false,
-        "mimeType": "multipart/form-data",
-        "data": form
+        "data": form,
+        "headers": {
+            'Content-Type': undefined
+        }
       }
 
-      $.ajax(settings).done(function (response) {
-        console.log(response);
-      });
+      $http(settings)
+        .then(function (d) {
+          console.log(d.data);
+        });
     }
   });
+// 'use strict';
+
+// angular.module('pocApp')
+//   .controller('AttachmentCtrl', function ($scope, $http) {
+
+//   })
+//   .controller('AttachmentUploadCtrl', function ($scope, $http, Upload) {
+//     $scope.uploadFiles = function(file, errFiles) {
+//         $scope.f = file;
+//         $scope.errFile = errFiles && errFiles[0];
+//         if (file) {
+//             file.upload = Upload.upload({
+//                 url: 'http://10.3.3.66:8080/alfresco/service/api/upload?alf_ticket=TICKET_decec884d65414733c9ddca657df2085791567d6',
+//                 data: {
+//                   filedata: file,
+//                   destination: "workspace://SpacesStore/7ca25e4f-c78e-490d-b3b7-db3fb7519e20",
+//                   containerid: "documentLibrary",
+//                   overwrite: "true"
+//                 }
+//             });
+
+//             file.upload.then(function (response) {
+//                 $timeout(function () {
+//                     file.result = response.data;
+//                 });
+//             }, function (response) {
+//                 if (response.status > 0)
+//                     $scope.errorMsg = response.status + ': ' + response.data;
+//             }, function (evt) {
+//                 file.progress = Math.min(100, parseInt(100.0 * 
+//                                          evt.loaded / evt.total));
+//             });
+//         }   
+//     }
+//   });
